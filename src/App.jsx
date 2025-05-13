9109da119a0a4ff27efb506ff3057886f5f64d9f
luizjuniorr <luiz.fernando.junior@eaportal.org>
2025-05-13 17:44:45 -0300

    Estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ export function App() {
 
   return(
     <div className="bg-slate-100 p-18 rounded-2xl flex flex-col items-center justify-center">
+      <h1 className="text-4xl mb-8 uppercase">Atividade Legal</h1>
       <Calculadora />
       <MostrarEsconder />
     </div>
